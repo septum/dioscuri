ae2cdda9ff12ebced4f2fd340df6c7f79b7e74f2
Orlando Valverde <orlando.jesig.va@gmail.com>
2025-03-04 01:10:07 -0600

    Improve the custom certificate verifier

--- src/skip_server_verification.rs
+++ src/skip_server_verification.rs
@@ -1,62 +1,72 @@
 use std::sync::Arc;
 
-use rustls::{client::danger, pki_types, SignatureScheme};
+use rustls::{
+    DigitallySignedStruct, RootCertStore, SignatureScheme,
+    client::{WebPkiServerVerifier, danger},
+    pki_types,
+};
 
 // From https://users.rust-lang.org/t/rustls-connecting-without-certificate-in-local-network/83822/5
+// and https://gist.github.com/doroved/2c92ddd5e33f257f901c763b728d1b61
 #[derive(Debug)]
-pub struct SkipServerVerification;
+pub struct SkipServerVerification {
+    inner: Arc<WebPkiServerVerifier>,
+}
 
 impl SkipServerVerification {
     pub fn new() -> Arc<Self> {
-        Arc::new(Self)
+        let roots = Arc::new(RootCertStore {
+            roots: webpki_roots::TLS_SERVER_ROOTS.to_vec(),
+        });
+        let inner = WebPkiServerVerifier::builder(roots).build().unwrap();
+        Arc::new(Self { inner })
     }
 }
 
 impl danger::ServerCertVerifier for SkipServerVerification {
+    fn verify_server_cert(
+        &self,
+        end_entity: &pki_types::CertificateDer<'_>,
+        intermediates: &[pki_types::CertificateDer<'_>],
+        server_name: &pki_types::ServerName<'_>,
+        ocsp: &[u8],
+        now: pki_types::UnixTime,
+    ) -> Result<danger::ServerCertVerified, rustls::Error> {
+        match self
+            .inner
+            .verify_server_cert(end_entity, intermediates, server_name, ocsp, now)
+        {
+            Ok(scv) => Ok(scv),
+            Err(rustls::Error::InvalidCertificate(cert_error)) => {
+                if let rustls::CertificateError::UnknownIssuer = cert_error {
+                    Ok(danger::ServerCertVerified::assertion())
+                } else {
+                    Err(rustls::Error::InvalidCertificate(cert_error))
+                }
+            }
+            Err(e) => Err(e),
+        }
+    }
+
     fn verify_tls12_signature(
         &self,
-        _message: &[u8],
-        _cert: &pki_types::CertificateDer<'_>,
-        _dss: &rustls::DigitallySignedStruct,
+        message: &[u8],
+        cert: &pki_types::CertificateDer<'_>,
+        dss: &DigitallySignedStruct,
     ) -> Result<danger::HandshakeSignatureValid, rustls::Error> {
-        Ok(danger::HandshakeSignatureValid::assertion())
+        self.inner.verify_tls12_signature(message, cert, dss)
     }
 
     fn verify_tls13_signature(
         &self,
-        _message: &[u8],
-        _cert: &pki_types::CertificateDer<'_>,
-        _dss: &rustls::DigitallySignedStruct,
+        message: &[u8],
+        cert: &pki_types::CertificateDer<'_>,
+        dss: &DigitallySignedStruct,
     ) -> Result<danger::HandshakeSignatureValid, rustls::Error> {
-        Ok(danger::HandshakeSignatureValid::assertion())
+        self.inner.verify_tls13_signature(message, cert, dss)
     }
 
     fn supported_verify_schemes(&self) -> Vec<SignatureScheme> {
-        vec![
-            SignatureScheme::RSA_PKCS1_SHA1,
-            SignatureScheme::ECDSA_SHA1_Legacy,
-            SignatureScheme::RSA_PKCS1_SHA256,
-            SignatureScheme::ECDSA_NISTP256_SHA256,
-            SignatureScheme::RSA_PKCS1_SHA384,
-            SignatureScheme::ECDSA_NISTP384_SHA384,
-            SignatureScheme::RSA_PKCS1_SHA512,
-            SignatureScheme::ECDSA_NISTP521_SHA512,
-            SignatureScheme::RSA_PSS_SHA256,
-            SignatureScheme::RSA_PSS_SHA384,
-            SignatureScheme::RSA_PSS_SHA512,
-            SignatureScheme::ED25519,
-            SignatureScheme::ED448,
-        ]
-    }
-
-    fn verify_server_cert(
-        &self,
-        _end_entity: &pki_types::CertificateDer<'_>,
-        _intermediates: &[pki_types::CertificateDer<'_>],
-        _server_name: &pki_types::ServerName<'_>,
-        _ocsp_response: &[u8],
-        _now: pki_types::UnixTime,
-    ) -> Result<danger::ServerCertVerified, rustls::Error> {
-        Ok(danger::ServerCertVerified::assertion())
+        self.inner.supported_verify_schemes()
     }
 }
