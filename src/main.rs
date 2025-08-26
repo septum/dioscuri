f259a729f0bc0430c74875e1ce8d3dba054424a1
Orlando Valverde <orlando.jesig.va@gmail.com>
2025-08-26 13:02:37 -0600

    Refactor main

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use gemini_client::GeminiClient;
 use ratatui::{
     DefaultTerminal, Frame,
-    layout::{Alignment, Constraint, Layout, Position},
+    buffer::Buffer,
+    layout::{Alignment, Constraint, Layout, Position, Rect},
     style::{Style, Stylize},
     text::{Line, Text},
     widgets::{
@@ -30,10 +31,25 @@ enum InputMode {
     Editing,
 }
 
+enum Status {
+    Running(bool),
+    Exit,
+}
+
 struct Input {
     value: String,
-    character_index: usize,
+    index: usize,
     mode: InputMode,
+}
+
+impl Default for Input {
+    fn default() -> Self {
+        Self {
+            value: String::from(DEFAULT_URL),
+            mode: InputMode::Editing,
+            index: DEFAULT_URL.len(),
+        }
+    }
 }
 
 struct App {
@@ -41,7 +57,6 @@ struct App {
     body: String,
     scroll: Scroll,
     input: Input,
-    exit: bool,
 }
 
 impl App {
@@ -50,25 +65,20 @@ impl App {
             client,
             body: String::new(),
             scroll: Scroll::default(),
-            input: Input {
-                value: String::from(DEFAULT_URL),
-                mode: InputMode::Editing,
-                character_index: DEFAULT_URL.len(),
-            },
-            exit: false,
+            input: Input::default(),
         }
     }
 
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
-        terminal.draw(|frame| self.draw(frame))?;
-
         let mut last_tick = Instant::now();
 
-        while !self.exit {
-            let timeout = UPDATE_TICK_RATE.saturating_sub(last_tick.elapsed());
+        self.render(terminal)?;
 
-            if self.handle_events(timeout)? {
-                terminal.draw(|frame| self.draw(frame))?;
+        while let Status::Running(should_render) =
+            self.handle_events(UPDATE_TICK_RATE.saturating_sub(last_tick.elapsed()))?
+        {
+            if should_render {
+                self.render(terminal)?;
             }
 
             if last_tick.elapsed() >= UPDATE_TICK_RATE {
@@ -79,142 +89,149 @@ impl App {
         Ok(())
     }
 
-    fn draw(&mut self, frame: &mut Frame) {
-        let app_title = Line::from(" dioscuri ".blue().bold());
-        let url_block = Block::bordered().title(app_title);
-        let url = Text::from(self.input.value.clone());
+    fn render(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
+        terminal.draw(|frame| self.draw_ui(frame))?;
+        Ok(())
+    }
 
-        let instructions = if self.input.mode == InputMode::Normal {
-            Line::from(" </> - Edit the address ".bold()).alignment(Alignment::Right)
-        } else {
-            Line::from(" <ENTER> - Request address | <ESC> - Focus the body ".bold())
-                .alignment(Alignment::Right)
-        };
-
-        let mut body_block = Block::bordered().title_bottom(instructions);
-
+    fn draw_ui(&mut self, frame: &mut Frame) {
         let [top, bottom] =
             Layout::vertical([Constraint::Length(3), Constraint::Fill(1)]).areas(frame.area());
 
-        let address_bar = Paragraph::new(url).block(url_block);
+        self.draw_address_bar(frame, top);
+        self.draw_body(frame.buffer_mut(), bottom);
+    }
+
+    fn draw_address_bar(&mut self, frame: &mut Frame, area: Rect) {
+        let title = Line::from(" dioscuri ".blue().bold());
+        let block = Block::bordered().title(title);
+        let url = Text::from(self.input.value.clone());
+
+        let address_bar = if self.input.mode == InputMode::Editing {
+            Paragraph::new(url).block(block).blue()
+        } else {
+            Paragraph::new(url).block(block)
+        };
+
+        address_bar.render(area, frame.buffer_mut());
 
         if self.input.mode == InputMode::Editing {
-            address_bar.blue().render(top, frame.buffer_mut());
             frame.set_cursor_position(Position::new(
-                top.x + self.input.character_index as u16 + 1,
-                top.y + 1,
+                area.x + self.input.index as u16 + 1,
+                area.y + 1,
             ));
-        } else {
-            address_bar.render(top, frame.buffer_mut());
-
-            body_block = body_block.border_style(Style::new().blue());
         }
+    }
 
-        let body_paragraph = Paragraph::new(self.body.replace("\t", " "))
-            .block(body_block)
+    fn draw_body(&mut self, buffer: &mut Buffer, area: Rect) {
+        let instructions = if self.input.mode == InputMode::Normal {
+            " <SLASH> - Edit the address "
+        } else {
+            " <ENTER> - Request address | <ESC> - Focus the body "
+        };
+        let instructions = Line::from(instructions.bold()).alignment(Alignment::Right);
+
+        let block = if self.input.mode == InputMode::Normal {
+            Block::bordered()
+                .title_bottom(instructions)
+                .border_style(Style::new().blue())
+        } else {
+            Block::bordered().title_bottom(instructions)
+        };
+
+        let paragraph = Paragraph::new(self.body.replace("\t", " "))
+            .block(block)
             .wrap(Wrap { trim: false })
             .scroll((self.scroll.value as u16, 0));
 
-        let body_lines = body_paragraph.line_count(bottom.width - 2);
-        let body_height = (bottom.height - 2) as usize;
+        let lines = paragraph.line_count(area.width - 2);
+        let height = (area.height - 2) as usize;
 
-        let pages = body_lines / body_height;
-        let reminder_lines = body_lines % body_height;
+        let pages = lines / height;
+        let reminder = lines % height;
 
-        self.scroll.max =
-            (body_height * pages.saturating_sub(1)) + if pages > 0 { reminder_lines } else { 0 };
-
+        self.scroll.max = (height * pages.saturating_sub(1)) + if pages > 0 { reminder } else { 0 };
         self.scroll.state = self.scroll.state.content_length(self.scroll.max);
 
-        body_paragraph.render(bottom, frame.buffer_mut());
+        paragraph.render(area, buffer);
 
         Scrollbar::new(ScrollbarOrientation::VerticalRight).render(
-            bottom,
-            frame.buffer_mut(),
+            area,
+            buffer,
             &mut self.scroll.state,
         );
     }
 
-    fn handle_events(&mut self, timeout: Duration) -> Result<bool> {
+    fn handle_events(&mut self, timeout: Duration) -> Result<Status> {
         if event::poll(timeout)? {
-            match self.input.mode {
-                InputMode::Normal => match event::read()? {
-                    Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                        match key_event.code {
-                            KeyCode::Esc => {
-                                self.exit = true;
-                            }
-                            KeyCode::Down => {
-                                if self.scroll.value < self.scroll.max {
-                                    self.scroll.state.scroll(ScrollDirection::Forward);
-                                    self.scroll.value = self.scroll.value.saturating_add(1);
-                                    return Ok(true);
-                                }
-                            }
-                            KeyCode::Up => {
-                                if self.scroll.value > 0 {
-                                    self.scroll.state.scroll(ScrollDirection::Backward);
-                                    self.scroll.value = self.scroll.value.saturating_sub(1);
-                                    return Ok(true);
-                                }
-                            }
-                            KeyCode::Char('/') => {
-                                self.input.mode = InputMode::Editing;
-                                self.input.character_index = self.input.value.len();
-                                return Ok(true);
-                            }
-                            _ => {}
-                        }
-                    }
-                    Event::Resize(_, _) => return Ok(true),
-                    _ => {}
-                },
-                InputMode::Editing => match event::read()? {
-                    Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                        match key_event.code {
-                            KeyCode::Enter => {
-                                self.request_url()?;
-                            }
-                            KeyCode::Char(to_insert) => self.enter_char(to_insert),
+            match event::read()? {
+                Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                    match self.input.mode {
+                        InputMode::Normal => match key_event.code {
+                            KeyCode::Up => self.scroll_up(),
+                            KeyCode::Down => self.scroll_down(),
+                            KeyCode::Char('/') => self.enter_editing_mode(),
+                            KeyCode::Esc => return Ok(Status::Exit),
+                            _ => return Ok(Status::Running(false)),
+                        },
+                        InputMode::Editing => match key_event.code {
+                            KeyCode::Enter => self.request_url()?,
+                            KeyCode::Char(char) => self.enter_char(char),
                             KeyCode::Backspace => self.delete_char(),
                             KeyCode::Left => self.move_cursor_left(),
                             KeyCode::Right => self.move_cursor_right(),
-                            KeyCode::Esc => {
-                                if self.body.is_empty() {
-                                    self.exit = true;
-                                } else {
-                                    self.input.mode = InputMode::Normal;
-                                }
-                            }
-                            _ => {
-                                return Ok(false);
-                            }
-                        };
-
-                        return Ok(true);
-                    }
-                    Event::Resize(_, _) => return Ok(true),
-                    _ => {}
-                },
+                            KeyCode::Esc => self.exit_editing_mode(),
+                            _ => return Ok(Status::Running(false)),
+                        },
+                    };
+                    return Ok(Status::Running(true));
+                }
+                Event::Resize(_, _) => return Ok(Status::Running(true)),
+                _ => return Ok(Status::Running(false)),
             }
         }
 
-        Ok(false)
+        Ok(Status::Running(false))
+    }
+
+    fn scroll_up(&mut self) {
+        if self.scroll.value > 0 {
+            self.scroll.state.scroll(ScrollDirection::Backward);
+            self.scroll.value = self.scroll.value.saturating_sub(1);
+        }
+    }
+
+    fn scroll_down(&mut self) {
+        if self.scroll.value < self.scroll.max {
+            self.scroll.state.scroll(ScrollDirection::Forward);
+            self.scroll.value = self.scroll.value.saturating_add(1);
+        }
+    }
+
+    fn exit_editing_mode(&mut self) {
+        if !self.body.is_empty() {
+            self.input.mode = InputMode::Normal;
+        }
+    }
+
+    fn enter_editing_mode(&mut self) {
+        self.input.mode = InputMode::Editing;
+        self.reset_cursor();
     }
 
     fn move_cursor_left(&mut self) {
-        let cursor_moved_left = self.input.character_index.saturating_sub(1);
-        self.input.character_index = self.clamp_cursor(cursor_moved_left);
+        let cursor_moved_left = self.input.index.saturating_sub(1);
+        self.input.index = self.clamp_cursor(cursor_moved_left);
     }
 
     fn move_cursor_right(&mut self) {
-        let cursor_moved_right = self.input.character_index.saturating_add(1);
-        self.input.character_index = self.clamp_cursor(cursor_moved_right);
+        let cursor_moved_right = self.input.index.saturating_add(1);
+        self.input.index = self.clamp_cursor(cursor_moved_right);
     }
 
-    fn enter_char(&mut self, new_char: char) {
+    fn enter_char(&mut self, char: char) {
         let index = self.byte_index();
-        self.input.value.insert(index, new_char);
+        self.input.value.insert(index, char);
         self.move_cursor_right();
     }
 
@@ -223,14 +240,14 @@ impl App {
             .value
             .char_indices()
             .map(|(i, _)| i)
-            .nth(self.input.character_index)
+            .nth(self.input.index)
             .unwrap_or(self.input.value.len())
     }
 
     fn delete_char(&mut self) {
-        let is_not_cursor_leftmost = self.input.character_index != 0;
+        let is_not_cursor_leftmost = self.input.index != 0;
         if is_not_cursor_leftmost {
-            let current_index = self.input.character_index;
+            let current_index = self.input.index;
             let from_left_to_current_index = current_index - 1;
 
             let before_char_to_delete = self.input.value.chars().take(from_left_to_current_index);
@@ -246,7 +263,7 @@ impl App {
     }
 
     fn reset_cursor(&mut self) {
-        self.input.character_index = 0;
+        self.input.index = self.input.value.len();
     }
 
     fn request_url(&mut self) -> Result<()> {
